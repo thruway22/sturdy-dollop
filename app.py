80182b1a93c404e0df01c800f72ff82c90eb6264
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 11:55:02 +0300

    u

--- app.py
+++ app.py
@@ -7,24 +7,7 @@ import plotly.graph_objects as go
 
 st.title('test')
 
-# df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
-# data = st.data_editor(df)
-
-options = {
-    'tvd': ['A', 'B'],
-    'hlt': ['A', 'B']
-}
-
-label_visibility = 'collapsed'
-
-tvd = st.selectbox(
-    'tvd', options['tvd'], label_visibility=label_visibility)
-
-hl = st.selectbox(
-    'hlt', options['hlt'], label_visibility=label_visibility)
-
 data = st.text_area('data')
-
 
 def convert_to_csv(input_string):
     values = input_string.split()
@@ -33,10 +16,48 @@ def convert_to_csv(input_string):
     df = pd.DataFrame(grouped_values, columns=['md', 'inc', 'azi'])
     return df
 
-# df = convert_to_csv(data)
-# st.write(df)
+df = convert_to_csv(data)
+st.write(df)
 
-# wp = wp.load(df)
-# st.plotly_chart(wp.plot(style={'color': 'dls', 'size': 5}))
+# df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
+# data = st.data_editor(df)
 
-# wp.plot(style={'color': 'dls', 'size': 5}).show()
+# data.dropna(axis=1, how='all', inplace=True)
+# data.dropna(inplace=True)
+
+
+##############################
+
+# df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
+# data = st.data_editor(df)
+
+# options = {
+#     'tvd': ['A', 'B'],
+#     'hlt': ['A', 'B']
+# }
+
+# label_visibility = 'collapsed'
+
+# tvd = st.selectbox(
+#     'tvd', options['tvd'], label_visibility=label_visibility)
+
+# hl = st.selectbox(
+#     'hlt', options['hlt'], label_visibility=label_visibility)
+
+# data = st.text_area('data')
+
+
+# def convert_to_csv(input_string):
+#     values = input_string.split()
+#     # Group every three values
+#     grouped_values = [values[i:i + 3] for i in range(0, len(values), 3)]
+#     df = pd.DataFrame(grouped_values, columns=['md', 'inc', 'azi'])
+#     return df
+
+# # df = convert_to_csv(data)
+# # st.write(df)
+
+# # wp = wp.load(df)
+# # st.plotly_chart(wp.plot(style={'color': 'dls', 'size': 5}))
+
+# # wp.plot(style={'color': 'dls', 'size': 5}).show()
