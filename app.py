4f6333dbb8c77eb51b4db67ddcc912e63620d216
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 11:07:49 +0300

    u

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.title('test')
 # df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
 # data = st.data_editor(df)
 
-tvd = st.selectbox('TVD', ['True', 'False'])
+tvd = st.selectbox('TVD', ['True', 'False'], label_visibility='collapsed')
 highlight = st.selectbox('Color', ['A', 'B', 'C'])
 data = st.text_area('data')
 tvd_true = st.toggle('TVD')
