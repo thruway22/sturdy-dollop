96d65a135b4a35f21c5bd30931c5dd1e6ab498c7
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 07:55:20 +0300

    u

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import pandas as pd
 
 st.title('test')
 
-df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
-data = st.data_editor(df)
+# df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
+# data = st.data_editor(df)
+
+data = st.text_area('data')
 
