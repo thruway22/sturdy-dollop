b3f4d8201bfdb55737d10bb9a8e33d6146083c99
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 12:49:20 +0300

    u

--- app.py
+++ app.py
@@ -1,49 +1,57 @@
-import utilities as utl
 import streamlit as st
 import pandas as pd
 import well_profile as wp
+from io import StringIO
+from math import degrees
 
-st.title('test2')
+import plotly.express as px
+import plotly.graph_objects as go
+
+def prep_data(string):
+    values = string.split()
+    grouped_values = [values[i:i + 3] for i in range(0, len(values), 3)]
+    df = pd.DataFrame(grouped_values, columns=['md', 'inc', 'azi'])
+    df.dropna(axis=1, how='all', inplace=True)
+    df.dropna(inplace=True)
+    return df
+
+def load_data(data):
+    trajectory = [{'md': 0, 'inc': 0, 'azi': 0, 'dl': 0, 'tvd': 0, 'north': 0, 'east': 0}]
+
+    for idx, row in data.iterrows():
+        if row['md'] > 0:
+            dogleg = calc_dogleg(trajectory[-1]['inc'], row['inc'], trajectory[-1]['azi'], row['azi'])
+            new_point = {
+                'md': row['md'], 
+                'inc': row['inc'], 
+                'azi': row['azi'],
+                'north': calc_north(trajectory[-1]['north'], trajectory[-1]['md'], row['md'], trajectory[-1]['inc'], row['inc'], trajectory[-1]['azi'], row['azi'], dogleg),
+                'east': calc_east(trajectory[-1]['east'], trajectory[-1]['md'], row['md'], trajectory[-1]['inc'], row['inc'], trajectory[-1]['azi'], row['azi'], dogleg),
+                'tvd': calc_tvd(trajectory[-1]['tvd'], trajectory[-1]['md'], row['md'], trajectory[-1]['inc'], row['inc'], dogleg),
+                'dl': degrees(dogleg)
+            }
+            trajectory.append(new_point)
+
+    return pd.DataFrame(trajectory[1:])  # Skip the initial dummy point
+
+def calc_dogleg(inc1, inc2, azi1, azi2):
+    if inc1 == inc2 and azi1 == azi2:
+        dl = 0
+    else:
+        inner_value = cos(radians(inc1)) * cos(radians(inc2)) + sin(radians(inc1)) * sin(radians(inc2)) * \
+            cos(radians(azi2 - azi1))
+        if inner_value > 1:
+            inner_value = 1
+        if inner_value < -1:
+            inner_value = -1
+        dl = acos(inner_value)
+    return dl
+
+
+st.title('test3')
 
 raw_data = st.text_area('data')
 
-df = utl.prep_data(raw_data)
-df = utl.load_data(df)
+df = prep_data(raw_data)
+df = load_data(df)
 st.write(df)
-
-
-##############################
-
-# df = pd.DataFrame([[0, 0, 0]], columns=["md", "inc", "azi"])
-# data = st.data_editor(df)
-
-# options = {
-#     'tvd': ['A', 'B'],
-#     'hlt': ['A', 'B']
-# }
-
-# label_visibility = 'collapsed'
-
-# tvd = st.selectbox(
-#     'tvd', options['tvd'], label_visibility=label_visibility)
-
-# hl = st.selectbox(
-#     'hlt', options['hlt'], label_visibility=label_visibility)
-
-# data = st.text_area('data')
-
-
-# def convert_to_csv(input_string):
-#     values = input_string.split()
-#     # Group every three values
-#     grouped_values = [values[i:i + 3] for i in range(0, len(values), 3)]
-#     df = pd.DataFrame(grouped_values, columns=['md', 'inc', 'azi'])
-#     return df
-
-# # df = convert_to_csv(data)
-# # st.write(df)
-
-# # wp = wp.load(df)
-# # st.plotly_chart(wp.plot(style={'color': 'dls', 'size': 5}))
-
-# # wp.plot(style={'color': 'dls', 'size': 5}).show()
