0cd2b339e5f17e5a9e6b9b4f1bee3b9285023c02
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 08:22:32 +0300

    u

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from io import StringIO
+import well_profile as wp
 
 st.title('test')
 
@@ -8,5 +10,31 @@ st.title('test')
 
 data = st.text_area('data')
 
-st.write(data)
+def convert_to_csv(input_string):
+    values = input_string.split()
+
+    # Group every three values
+    grouped_values = [values[i:i + 3] for i in range(0, len(values), 3)]
+
+    # Create a DataFrame from the grouped values
+    df = pd.DataFrame(grouped_values, columns=['a', 'b', 'c'])
+
+    # # Export DataFrame to CSV file
+    # df.to_csv('output.csv', index=False)
+
+
+st.write(convert_to_csv(data))
+
+
+# def input_to_csv(input_string):
+#     # Convert string to StringIO object (which behaves like a file object)
+#     string_io = StringIO(input_string)
+
+#     # Read the string as a CSV into a DataFrame
+#     df = pd.read_csv(string_io, sep=",")
+
+#     # Export DataFrame to CSV file
+#     df.to_csv("output.csv", index=False)
+
+# st.write(data)
 
