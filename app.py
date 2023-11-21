d74cc370c9d0e18683f1d034d73938fd1a6356c8
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 12:28:41 +0300

    u

--- app.py
+++ app.py
@@ -1,28 +1,13 @@
 import streamlit as st
 import pandas as pd
-from io import StringIO
 import well_profile as wp
-import plotly.express as px
-import plotly.graph_objects as go
+import utilities
 
 st.title('test')
 
 data = st.text_area('data')
 
-def create_df(input_string):
-    values = input_string.split()
-    # Group every three values
-    grouped_values = [values[i:i + 3] for i in range(0, len(values), 3)]
-    df = pd.DataFrame(grouped_values, columns=['md', 'inc', 'azi'])
-    return df
-
-def process_df(df):
-    df.dropna(axis=1, how='all', inplace=True)
-    df.dropna(inplace=True)
-    
-    return df
-
-df = convert_to_csv(data)
+df = process_df(create_df(data))
 st.write(df)
 
 
