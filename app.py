7ef6b4e78a9d6bc0e6859e6037040bebc9b4e101
thruway22 <121792615+thruway22@users.noreply.github.com>
2023-11-21 15:26:40 +0300

    u

--- app.py
+++ app.py
@@ -69,9 +69,9 @@ def calc_rf(dogleg):
 def plot_data(df, highlight_dls):
     if highlight_dls:
         custom_colorscale = [
-        [0.0, 'rgb(255,255,255)'],  # white
-        [0.5, 'rgb(255,150,150)'],  # light red
-        [1.0, 'rgb(255,0,0)']       # red
+        [0.0, 'rgb(188,33,40)']
+        [0.5, 'rgb(243,164,112)'],  # light red
+        [1.0, 'rgb(177,177,177)']       # red
         ]
 
         fig = go.Figure(
